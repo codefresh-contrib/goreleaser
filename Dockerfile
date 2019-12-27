74d69d44bb1074df0036f11bd0128cab7d381f85
alex-codefresh <a.cheshko@codefresh.io>
2019-12-27 15:39:31 +0300

    Change the base image Add the needed dependencies Add an entrypoint script Get GITHUB_TOKEN automatically from the trigger Automatically put the git in clean state

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,27 @@
+ARG GOLANG_VERSION=1.13
+
 FROM alpine:3.11 as build
 
-RUN apk add curl && \
-    curl -sfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | sh
+RUN apk add curl wget 
 
-FROM alpine:3.11 as runtime
+RUN curl -sfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | sh 
 
+ARG CF_CLI_VERSION=v0.35.0
+
+RUN wget https://github.com/codefresh-io/cli/releases/download/v0.35.0/codefresh-${CF_CLI_VERSION}-alpine-x64.tar.gz -O - \
+  | tar -xzf - -C /bin 
+
+FROM golang:${GOLANG_VERSION}-alpine3.11 as runtime
+
+WORKDIR /step
+
+RUN apk add --no-cache go git jq libgcc libstdc++
+
+COPY --from=build /bin/codefresh /bin/codefresh
 COPY --from=build /bin/goreleaser /bin/goreleaser
 
-ENTRYPOINT ["/bin/goreleaser"]
+COPY entrypoint.sh .
+
+ENTRYPOINT ["/bin/sh", "/step/entrypoint.sh"]
+
+CMD ["goreleaser"]
---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+# !/bin/bash
+
+set -e
+
+bold() { echo -e "\e[1m$@\e[0m" ; }
+red() { echo -e "\e[31m$@\e[0m" ; }
+green() { echo -e "\e[32m$@\e[0m" ; }
+ok() { green OK ; }
+
+function getContextFromTrigger() {
+    checkTrigger || return 1
+    GIT_CONTEXT=$(codefresh get pipeline "$CF_PIPELINE_NAME" -o json | jq --arg triggerId "${CF_PIPELINE_TRIGGER_ID}" -r '.spec.triggers[] | select(.id == $triggerId) | .context') || return 1 
+    export GIT_CONTEXT
+}
+
+function getTokenFromContext() {
+    bold "Getting a git token from the context \"${GIT_CONTEXT}\"..."
+    GITHUB_TOKEN=$(codefresh get contexts --type git.github ${GIT_CONTEXT} --decrypt -o json | jq -r '.spec.data.auth.password') || return 1
+    export GITHUB_TOKEN
+    ok
+}
+
+function checkTrigger() {
+    if [ -z "$CF_PIPELINE_TRIGGER_ID" ]; then
+        red "Failed to get the trigger data - the pipeline hasn't been started by a trigger"
+        return 1
+    fi
+}
+
+getContextFromTrigger
+getTokenFromContext
+
+git reset --hard
+git clean -df
+
+$@
